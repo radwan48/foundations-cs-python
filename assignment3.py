9812068a63a5f55d372ceea8233bd3ab42b43a9a
radwan48 <radwanramadan82@gmail.com>
2023-10-27 15:29:48 +0300

    starting with assignment3

--- assignment3.py
+++ assignment3.py
@@ -19,17 +19,62 @@ def addMatrices(matrix1, matrix2):
         result.append(row)
     return result
 
+
+
+
+
+
 def displayMatrix(matrix):
-     for row in matrix:
-         print(row)
+    for row in matrix:
+     print(row)
 
 
-rows = int(input("Enter number of rows :"))
-col = int(input("Enter number of col :"))
 
-print("Enter elements of the first matrix")
-matrix1 = get_matrix(rows, col)
-print("Enter elements of the second matrix")
-matrix2 = get_matrix(rows, col)
-print("The sum of two matrices")
-result_matrix = addMatrices(matrix1, matrix2)
+
+
+
+def displayMenu():
+    print("1.Add matrices\n"
+          + "2.Check Rotation\n"
+          + "3.Invert Dictionary\n"
+          + "4.Convert Matrix to Dictionary\n"
+          + "5.Check Palindrome\n"
+          + "6. Search for an Element & Merge Sort\n"
+          + "7.Exit")
+displayMenu()
+choice = input("Enter your choice :")
+if choice == "1":
+   rows = int(input("Enter number of rows :"))
+   col = int(input("Enter number of col :"))
+   print("Enter elements of the first matrix")
+   matrix1 = get_matrix(rows, col)
+   print("Enter elements of the second matrix")
+   matrix2 = get_matrix(rows, col)
+   print("The sum of two matrices")
+   result_matrix = addMatrices(matrix1, matrix2)
+   print(result_matrix)
+   displayMenu()
+   choice = input("Enter your choice :")
+elif choice == "2":
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
