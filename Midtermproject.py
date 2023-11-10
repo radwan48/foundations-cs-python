33182792803b6723fe74145358e53074fe0269bc
radwan48 <radwanramadan82@gmail.com>
2023-11-10 02:59:37 +0200

    adding function for main() for choices and displaying menu

---
+++ Midtermproject.py
@@ -0,0 +1,38 @@
+
+def displayMenu():
+    print("1. Open Tab\n"
+        + "2. Close Tab\n"
+        + "3. Switch Tab\n"
+        + "4. Display All Tabs\n"
+        + "5. Open Nested Tab\n"
+        + "6. Clear All Tabs\n"
+        + "7. Save Tabs\n"
+        + "8. Import Tabs\n"
+        + "9. Exit")
+
+
+def main():
+    displayMenu()
+    choice = input("Enter your choice :")
+    while choice != "9":
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            pass
+        elif choice == "7":
+            pass
+        elif choice == "8":
+            pass
+        else:
+            print("invalid input, please try again")
+            main()
+
+
