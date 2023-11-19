cac3707dbcdc2cf229ee59fb660a965251374515
radwan48 <radwanramadan82@gmail.com>
2023-11-19 11:20:58 +0200

    add displayMenu function

---
+++ assignment4.py
@@ -0,0 +1,20 @@
+def displayMenu():
+    print("1. Singly Linked List\n"
+        + "2. Check if Palindrome\n"
+        + "3. Priority Queue\n"
+        + "4. Evaluate an Infix Expression\n"
+        + "5. Graph\n"
+        + "6. Exit")
+
+
+
+
+
+
+
+
+
+
+
+
+
