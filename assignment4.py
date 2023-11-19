62c8aca71d7379b8abe3025514b9b74ea4f9b54a
radwan48 <radwanramadan82@gmail.com>
2023-11-19 11:26:29 +0200

    added choice menu

--- assignment4.py
+++ assignment4.py
@@ -13,6 +13,21 @@ def main():
     while True:
         displayMenu()
         choice = input("Enter your choice :")
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            exit()
+        else:
+            print("Invalid choice, please enter the correct choice.")
+
 
 
 
