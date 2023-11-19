575d3ba548c95516f98ff584708f4383f8b97154
radwan48 <radwanramadan82@gmail.com>
2023-11-19 12:08:05 +0200

    added Linkedlist class

--- assignment4.py
+++ assignment4.py
@@ -16,6 +16,10 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+        self.size = 0
 
 
 
